40396e00a88fb735440532e420ca98cc9b220b4d
Maksym Hysko <hysko.mv@gmail.com>
2026-03-18 14:04:17 +0200

    ci: fix workflow issues

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { HeadersConfiguratorInterceptor } from './components/headers.configurato
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import fastifyCookie from '@fastify/cookie';
 import fastifyHttpProxy from '@fastify/http-proxy';
-import session from '@fastify/session';
+session from '@fastify/session';
 import { GlobalExceptionFilter } from './components/global-exception.filter';
 import * as os from 'os';
 import { readFileSync, readFile, readdirSync } from 'fs';
@@ -95,6 +95,19 @@ async function bootstrap() {
             )
           }
         : null
+  });
+
+  // Add /api/config handler before setDefaultRoute
+  server.get('/api/config', async (req, res) => {
+    res.statusCode = 200;
+    res.header('Content-Type', 'application/json');
+    res.send({
+      URL: process.env.URL,
+      GRPC_WEB_PROXY_URL: process.env.GRPC_WEB_PROXY_URL,
+      CHAT_API_URL: process.env.CHAT_API_URL,
+      CHAT_API_MODEL: process.env.CHAT_API_MODEL,
+      CHAT_API_MAX_TOKENS: process.env.CHAT_API_MAX_TOKENS
+    });
   });
 
   server.setDefaultRoute((req, res) => {
